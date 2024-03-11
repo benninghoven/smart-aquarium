69752ca0c183353dd14364a83d66122cfce29fdd
StudentAccount1000 <89495655+StudentAccount1000@users.noreply.github.com>
2024-03-11 14:23:18 -0700

    supports account login and creation

--- src/database/flasktesting.py
+++ src/database/flasktesting.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect
 from sql import get_db_and_cursor
-#from flask_mysqldb import MySQL
+import validate_account
 
 app = Flask(__name__)
 '''mysql = MySQL()
@@ -19,29 +19,62 @@ def index():
         query_result = []
         try:
             db, cur = get_db_and_cursor()
-            cur.execute("SELECT * FROM SENSOR_READINGS WHERE TIMESTAMPDIFF(DAY, SENSOR_READINGS.TIMESTP, NOW()) < 1;") # takes all readings from last day
+            cur.execute("SELECT * FROM SENSOR_READINGS WHERE TIMESTAMPDIFF(DAY, SENSOR_READINGS.TIMESTP, NOW()) < 8 LIMIT 20;") # takes all readings from last day
             query_result = cur.fetchall()
-            print(query_result)
+            #print(query_result)
             #db.commit()
             cur.close()
         except Exception as e: 
             print(e)
 
 
-        return redirect('/')###, tasks=query_result)
-    else:
-        query_result = []
-        try:
-            db, cur = get_db_and_cursor()
-            cur.execute("SELECT * FROM SENSOR_READINGS WHERE TIMESTAMPDIFF(DAY, SENSOR_READINGS.TIMESTP, NOW()) < 1;") # takes all readings from last day
-            query_result = cur.fetchall()
-            print(query_result)
-            #db.commit()
-            cur.close()
-        except Exception as e: 
-            print(e)
         return render_template("results.html", tasks=query_result)
+    else:
+        return render_template("results.html", tasks=[])
     
+
+# account logins and creation
+
+@app.route('/get_login', methods=['GET'])
+def get_login():
+    return render_template("login.html")
+
+
+@app.route('/handle_login', methods=["POST", "GET"])
+def handle_login():
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+
+        login_success = validate_account.login(username, password)
+        print(login_success)
+        if login_success:
+            return redirect('/')
+        else:
+            return render_template("login.html")
+    else:
+        return render_template("login.html")
+
+
+@app.route('/get_account_creation_page', methods=["GET"])
+def get_account_creation_page():
+    return render_template("create_account.html")
+
+
+@app.route('/handle_account_creation', methods=["POST"])
+def handle_account_creation():
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+
+        creation_success = validate_account.create_account(username, password)
+        print(creation_success)
+        if creation_success:
+            return redirect('/handle_login')
+        else:
+            return render_template("create_account.html")
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
