d0a0f7eb920a2731f1cfa75b98d9ffe41a353b22
StudentAccount1000 <89495655+StudentAccount1000@users.noreply.github.com>
2024-03-11 17:00:24 -0700

    Update fishology.sql

--- src/database/fishology.sql
+++ src/database/fishology.sql
@@ -9,15 +9,17 @@ USE FISHOLOGY;
 DROP TABLE accounts;
 CREATE TABLE IF NOT EXISTS accounts(
 username		VARCHAR(20) NOT NULL,
-hashed_pw		CHAR(255),
-salt_val		CHAR(16),
+hashed_pw		CHAR(60),
+salt_val		CHAR(29),
 PRIMARY KEY(username)
 );
 
-
+SELECT * FROM ACCOUNTS;
+insert into accounts VALUES ('acc', '13851-238203-11410-4', '121441');
 
 # Need to check if the sensor data is in float form
 # test the NOW() function, may need to create a trigger
+DROP TABLE sensor_readings;
 CREATE TABLE IF NOT EXISTS SENSOR_READINGS(
 username		VARCHAR(20) NOT NULL,
 timestp 		DATETIME NOT NULL DEFAULT NOW(),
@@ -42,7 +44,7 @@ PRIMARY KEY(fish_name)
 
 
 INSERT INTO SENSOR_READINGS (username, timestp, water_temp, PPM, pH) VALUES
-("TESTUSER", '2024-02-24 11:46:21', 74.3, 50, 6.73);
+("TESTUSER", '2024-03-4 1:07:20', 71.3, 50, 6.94);
 
 # ALL READINGS
 SELECT * FROM SENSOR_READINGS;
