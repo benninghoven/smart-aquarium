e55c87ac6c5c419262d96ec64991a0b67a3a172e
benninghoven <devinbenninghoven@gmail.com>
2024-03-18 00:08:31 -0700

    cleaned up excess flutter files

--- src/flutter/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/arm64/Pods-Runner-dummy.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/devin/sandbox/spring_2024/smart-aquarium/src/flutter/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap \
-  /Users/devin/sandbox/spring_2024/smart-aquarium/src/flutter/macos/Pods/Target\ Support\ Files/Pods-Runner/Pods-Runner-dummy.m
--- src/flutter/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/arm64/Pods_Runner_vers.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/devin/sandbox/spring_2024/smart-aquarium/src/flutter/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap \
-  /Users/devin/sandbox/spring_2024/smart-aquarium/src/flutter/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/DerivedSources/Pods_Runner_vers.c
